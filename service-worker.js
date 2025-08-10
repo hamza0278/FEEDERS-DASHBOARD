61c69c8249d474a22c69ecb32ddd285832fe6282
hamza0278 <hk1001762@gmail.com>
2025-08-10 12:07:34 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,19 @@
+const CACHE_NAME = 'feeder-dashboard-cache-v1';
+const urlsToCache = [
+  '/',
+  '/FEEDERS-DASHBOARD/index.html',
+  '/FEEDERS-DASHBOARD/manifest.json',
+  '/FEEDERS-DASHBOARD/icons/icon-192.png',
+  '/FEEDERS-DASHBOARD/icons/icon-512.png',
+  // add any other assets like CSS/JS files here
+];
+
 self.addEventListener('install', event => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('activate', event => {
